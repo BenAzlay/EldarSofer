be434ddb09481222cf7109e1f66bc7b2a01053cf
BenAzlay <benjamin.azoulay@protonmail.com>
2024-05-16 21:26:41 +0300

    fleek config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'out',
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;

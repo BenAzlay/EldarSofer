9903dbb0d3b16109ac484c9f160a1b056f644314
BenAzlay <benjamin.azoulay@protonmail.com>
2024-05-19 00:55:33 +0300

    Read button font size decrease

--- src/app/constants.js
+++ src/app/constants.js
@@ -8,7 +8,7 @@ export default {
         },
         // {
         //     title: "The Golden Prince",
-        //     summary: "In Ancient China, renowned master Gu-Tzu can give pilgrims anything they desire. A potion to cure any ailment of the body or soul, a sword to cut through steel like flesh, a mirror to reflect one's true self... The old master alchemist could produce it all, never asking for any money in return. Yet he does require something else in return, something seemingly benign, but perhaps more valuable than money, and much more powerful...",
+        //     summary: "",
         //     pdfPath: "/Master_Guzi.pdf",
         //     coverImage: require('@/app/assets/GoldenPrinceCover.png'),
         // },

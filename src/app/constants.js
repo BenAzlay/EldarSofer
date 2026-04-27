1483ac3e7a99f73ccc0ff541fa1c559dbfed136e
BenAzlay <benjamin.azoulay@protonmail.com>
2026-04-27 18:18:01 +0200

    Updated stories

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,22 +1,22 @@
 export default {
     stories: [
         {
-            title: "Master Gu-Tzu",
-            summary: "In Ancient China, renowned master Gu-Tzu can give pilgrims anything they desire. A potion to cure any ailment of the body or soul, a sword to cut through steel like flesh, an invisibility blanket... The old master alchemist can produce it all, never asking for any money in return. Yet he does require something else in return, something seemingly benign, but perhaps more valuable than money, and much more powerful...",
-            pdfPath: "/Master_Guzi.pdf",
-            coverImage: require('@/app/assets/MasterGuTzuCover.png'),
-        },
-        {
-            title: "Yantra",
-            summary: "In the great Indian kingdom of Magadha, people don't need to work anymore, as the Yantras, powerful and intelligent machines, were created to be better soldiers, farmers, doctors... Only the Brahmins haven't let the Yantras take their place, and hold absolute power by maintaining the people in debilitating idleness. Alone against the Brahmins, princess Gargi refuses to live an idle, meaningless life. But the Yantras won't make a rebellion easy...",
-            pdfPath: "/Yantra.pdf",
+            title: "Twice-Born",
+            summary: "In Magadha, the yantras work as farmers, soldiers, physicians... Thanks to these machines, there is no more hunger, no more wars, no more castes. But in this never-before-seen age of abundance, do people know how to be free?",
+            pdfPath: "/Twice-Born.pdf",
             coverImage: require('@/app/assets/YantraCover.png'),
         },
         {
-            title: "The Golden Prince",
-            summary: "The fabulously rich ruler of Mali, Mansa Musa, gives away gold in abundance to everyone on his way to Mecca. But his young son, prince Maghan, senses this gold won't make people happy for long...",
-            pdfPath: "/The_Golden_Prince.pdf",
+            title: "Where Gold Wept",
+            summary: "The fabulously rich ruler of Mali, Mansa Musa, gives away gold in abundance to everyone on his way to Mecca. But his young son, Prince Maghan, senses this gold won't make people happy for long...",
+            pdfPath: "/Where_Gold_Wept.pdf",
             coverImage: require('@/app/assets/GoldenPrinceCover.png'),
+        },
+        {
+            title: "Master Gu-Tzu",
+            summary: "In ancient China, renowned master Gu-Tzu can give pilgrims anything they desire. A potion to cure any ailment of the body or soul, a sword to cut through steel like flesh, an invisibility blanket... The old master alchemist can produce it all, never asking for any money in return. Yet he does require something else, something seemingly benign, but perhaps more valuable than money, and much more powerful...",
+            pdfPath: "/Master_Guzi.pdf",
+            coverImage: require('@/app/assets/MasterGuTzuCover.png'),
         },
     ],
 };
